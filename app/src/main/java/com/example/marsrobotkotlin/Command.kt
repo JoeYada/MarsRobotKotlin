608682065cf8e94f2dc101327dfac8f50678f230
Joseph A <user@example.com>
2019-02-26 15:39:51 +0000

    Added Logic for Commands

--- app/src/main/java/com/example/marsrobotkotlin/Command.kt
+++ app/src/main/java/com/example/marsrobotkotlin/Command.kt
@@ -5,21 +5,38 @@ sealed class Command {
 
     object TurnLeft : Command() {
         override fun execute(robot: Robot): Robot {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            when(robot.getCurrentDirection()) {
+                Direction.NORTH -> robot.setCurrentDirection(Direction.WEST)
+                Direction.SOUTH -> robot.setCurrentDirection(Direction.EAST)
+                Direction.EAST -> robot.setCurrentDirection(Direction.NORTH)
+                Direction.WEST -> robot.setCurrentDirection(Direction.SOUTH)
+            }
+            return robot
         }
-
     }
 
     object TurnRight : Command() {
         override fun execute(robot: Robot): Robot {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            when(robot.getCurrentDirection()) {
+                Direction.NORTH -> robot.setCurrentDirection(Direction.EAST)
+                Direction.SOUTH -> robot.setCurrentDirection(Direction.WEST)
+                Direction.EAST -> robot.setCurrentDirection(Direction.SOUTH)
+                Direction.WEST -> robot.setCurrentDirection(Direction.NORTH)
+            }
+            return robot
         }
 
     }
 
     object GoForward: Command() {
         override fun execute(robot: Robot): Robot {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            when(robot.getCurrentDirection()) {
+                Direction.NORTH -> robot.setYPos(robot.getYPos() + 1)
+                Direction.SOUTH -> robot.setYPos(robot.getYPos() - 1)
+                Direction.EAST -> robot.setXPos(robot.getXPos() + 1)
+                Direction.WEST -> robot.setXPos(robot.getXPos() - 1)
+            }
+            return robot
         }
 
     }
--- app/src/main/java/com/example/marsrobotkotlin/Robot.kt
+++ app/src/main/java/com/example/marsrobotkotlin/Robot.kt
@@ -1,8 +1,33 @@
 package com.example.marsrobotkotlin
 
 class Robot(private var currentDirection: Direction,
-            private var XPos: Int,
-            private var YPos: Int,
+            private var xPos: Int,
+            private var yPos: Int,
             private val commands: List<Command>) {
+
+    fun getCurrentDirection() = currentDirection
+
+    fun setCurrentDirection(direction: Direction) {
+        currentDirection = direction
+    }
+
+    fun getXPos() = xPos
+
+    fun setXPos(newPos: Int) {
+        xPos = newPos
+    }
+
+    fun getYPos() = yPos
+
+    fun setYPos(newPos: Int) {
+      yPos = newPos
+    }
+
+    fun executeCommandsAndGetState(): String {
+        for (command in commands) {
+            command.execute(this)
+        }
+        return "$xPos $yPos ${currentDirection.name[0]}"
+    }
 
 }
