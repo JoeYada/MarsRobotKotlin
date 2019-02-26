12dbf8f8d54f0d6797e8c174ba1279de32d2394d
Joseph A <user@example.com>
2019-02-26 16:06:47 +0000

    Added data class for Edge

---
+++ app/src/main/java/com/example/marsrobotkotlin/Edge.kt
@@ -0,0 +1,6 @@
+package com.example.marsrobotkotlin
+
+data class Edge(private val xFloor: Int,
+                private val xCeiling: Int,
+                private val yFloor: Int,
+                private val yCeiling: Int)
